d35b9a19ee264aff968bbea47238d7a95465e9d4
Cc <85765066@qq.com>
2019-09-03 15:49:08 +0800

    .

--- drawlibrary/src/main/java/com/eju/cy/drawlibrary/bean/RoomDataDto.kt
+++
@@ -1,8 +0,0 @@
-package com.eju.cy.drawlibrary.bean;
-
-public class RoomDataDto {
-
-    var method: String = String()
-    var data: String = String()
-
-}
--- drawlibrary/src/main/java/com/eju/cy/drawlibrary/web/ArInterface.java
+++
@@ -1,6 +0,0 @@
-package com.eju.cy.drawlibrary.web;
-
-public interface ArInterface {
-
-    void pushMsg(String msg);//与H5交互
-}
